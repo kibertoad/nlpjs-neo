e70022b8b68ca1259e57eccc3f147ec9c06335c3
Igor Savin <iselwin@gmail.com>
2026-09-21 01:21:41 +0300

    Use newlines in pipeline regression fixture

--- packages/core/test/container.test.ts
+++ packages/core/test/container.test.ts
@@ -316,7 +316,9 @@ describe('Container', () => {
     test('String pipelines ignore whitespace-only lines', async () => {
       const instance = new Container();
       instance.loadPipelinesFromString(
-        '# Pipelines\\n## main\\nset input.value 1\\n  \\nget input.value'
+        ['# Pipelines', '## main', 'set input.value 1', '  ', 'get input.value'].join(
+          '\n'
+        )
       );
 
       const actual = await instance.runPipeline('main', {}, new Other());
