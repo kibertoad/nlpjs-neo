3e4b3f4c98d435bf928adcd2ba46b7e35b0fdbfa
Igor Savin <iselwin@gmail.com>
2026-09-21 03:28:25 +0300

    Stabilize serialized settings formatting

--- packages/core/src/clonable.ts
+++ packages/core/src/clonable.ts
@@ -92,8 +92,8 @@ class Clonable {
           }
         } else if (typeof fn === 'boolean') {
           if (fn) {
-            result[key] =
-              key === 'settings' ? { ...this[key] } : this[key];
+            const value = this[key];
+            result[key] = key === 'settings' ? { ...value } : value;
             if (key === 'settings') {
               delete (result[key] as Settings).container;
             }
